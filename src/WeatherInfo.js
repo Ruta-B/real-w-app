d717bcba9adefdc5685658138e259ad1a8749c92
Ruta-B <baumaneruta38@gmail.com>
2025-02-16 20:33:41 +0100

    update project

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,55 +1,51 @@
 import React from "react";
 import Time from "./Time";
-
+import "./WeatherInfo.css";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-    return (
-      <div className="WeatherInfo">
-        <h1>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="48px"
-            viewBox="0 -960 960 960"
-            width="48px"
-            fill="#5f6368"
-          >
-            <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
-          </svg>
-          {props.data.city}, {props.data.country}
-        </h1>
-        <ul>
-          <li>
-            <Time date={props.data.date} />
-          </li>
-          <li className="text-capitalize">{props.data.description}</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <div className="d-flex ">
-              <div>
-                <img
-                  src={props.data.icon_url}
-                  alt={props.data.iconDescription}
-                  className="img-float "
-                />
-              </div>
-              <div>
-                <span className="temperature ">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="unit">°C</span>{" "}
-              </div>
+  return (
+    <div className="WeatherInfo">
+      <h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="48px"
+          viewBox="0 -960 960 960"
+          width="48px"
+          fill="#5f6368"
+        >
+          <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
+        </svg>
+        {props.data.city}, {props.data.country}
+      </h1>
+      <ul>
+        <li>
+          <Time date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6 main-temp">
+          <div className="d-flex ">
+            <div>
+              <img
+                src={props.data.icon_url}
+                alt={props.data.iconDescription}
+                className="img-float "
+              />
             </div>
-          </div>{" "}
-          <div className="col-6">
-            <ul>
-              <li>Humidity: {props.data.humidity} %</li>
-              <li>Wind: {props.data.wind} km/h</li>
-              <li>Sunrise: 5:00</li>
-              <li>Sunset: 16:00</li>
-            </ul>
+            <div>
+              <WeatherTemperature celsius={props.data.temperature} />
+            </div>
           </div>
+        </div>{" "}
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity} %</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
         </div>
       </div>
-    );
+    </div>
+  );
 }
